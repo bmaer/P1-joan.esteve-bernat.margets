83d54c83646438c9491fca6fd7a09c76bf0aa1eb
Bernat Margets <bernatmargets@gmail.com>
2025-11-17 18:09:50 +0100

    Unos ajustes al menu de entrada

--- src/Presentation/UserInterface.java
+++ src/Presentation/UserInterface.java
@@ -6,7 +6,12 @@ public class UserInterface {
     }
 
     public void startUp(){
-        System.out.println("Starting...");
+        System.out.println(
+                " _  _         _       ___           ___          _        _\n" +
+                "| \\| |_  _ __| |___  | __|  _ ___  | _ \\_ _ ___ (_)___ __| |_\n" +
+                "| .` | || / _` / -_) | _| || / -_) |  _/ '_/ _ \\| / -_) _|  _|\n" +
+                "|_|\\_|\\_,_\\__,_\\___| |___\\_, \\___| |_| |_| \\___// \\___\\__|\\__|\n" +
+                "                         |__/                 |__/");
     }
     public void showError(){
         System.out.println("Error file not found");
--- src/main.java
+++ src/main.java
@@ -6,10 +6,12 @@ import Presentation.UserInterface;
 
 public class main {
     public static void main(String[] args) {
-        Class1DAO dao = new Class1JSON();
+        //Class1DAO dao = new Class1JSON();
+        //UserInterface ui = new UserInterface();
+        //Class1Manager manager = new Class1Manager(dao);
+        //Controller controller = new Controller(ui, manager);
+        //controller.run();
         UserInterface ui = new UserInterface();
-        Class1Manager manager = new Class1Manager(dao);
-        Controller controller = new Controller(ui, manager);
-        controller.run();
+        ui.startUp();
     }
 }
